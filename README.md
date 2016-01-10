dab75478aa9510470ac032157eab7293a1d1252c
Paulo Santos <sungonku@gmail.com>
2016-01-10 23:03:50 +0000

    first commit (after week 2)

---
+++ README.md
@@ -0,0 +1,19 @@
+Copenhagen
+
+Introduction to Meteor.js Development
+by University of London, Goldsmiths, University of London
+
+My pratice during lessons.
+
+Week 1:
+Install Meteor tools
+Create and run a Meteor application
+Edit a template
+Define a template helper
+Define template event listeners
+
+Week 2:
+Create Mongo Collections
+Use Mongo find and insert operations
+Control a Bootstrap modal from Meteor
+Use third party Meteor packages to add functionality
---
+++ copenhagen.js
@@ -0,0 +1,64 @@
+Images = new Mongo.Collection("images");
+
+
+if (Meteor.isClient) {
+
+  Template.images.helpers({
+    images: function () {
+        var all = Images.find({}, {sort:{createdOn: -1, rating:-1}}).fetch();
+        var chunks = [];
+        var size = 4;
+
+        while (all.length > size) {
+            chunks.push({ row: all.slice(0, size)});
+            all = all.slice(size);
+        }
+        chunks.push({row: all});
+        
+        return chunks;
+    }
+  });
+  
+  Template.images.events({
+    "click .js-image": function(event){
+      var image_src = $(event.currentTarget).attr("src");
+      $('#image_modal').attr("src", image_src);
+      $('#imageModal').modal('show');
+    },
+    'click .js-del-image': function(event){
+      var image_id = this._id;
+      $("#"+image_id).hide('slow', function(){
+        Images.remove({"_id": image_id});
+      })
+    },
+    'click .js-rate-image': function(event){
+      var image_id = this.id;
+      var rating = $(event.currentTarget).data("userrating");
+      console.log(image_id);
+      console.log(rating);
+      Images.update({_id:image_id}, {$set: {rating:rating}});
+    },
+    'click .js-image-add': function(event){
+      $("#image_add_form").modal('show');
+    }
+  });
+  
+  Template.image_add_form.events({
+    'submit .js-add-image':function(event){
+
+      Images.insert({
+        img_src: event.target.img_src.value,
+        img_alt: event.target.label.value,
+        label: event.target.label.value,
+        description: event.target.description.value,
+        createdOn: new Date()
+      });
+      $("#image_add_form").modal('hide');
+      return false;
+    },
+    'click .js-close-form':function(event){
+      $("#image_add_form").modal('hide');
+    }
+  });
+  
+}
